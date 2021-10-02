db280dce604090a0720804287611c5c6b864b710
Bond <bondarenko.jenek@mail.ru>
2021-10-02 16:20:59 +0300

    edited admin-panel

--- catalog/models.py
+++ catalog/models.py
@@ -18,12 +18,18 @@ class Book(models.Model):
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
                                       '">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
+
 
 
 class BookInstance(models.Model):
@@ -59,3 +65,10 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter the book\'s natural language')
+
+    def __str__(self):
+        return self.name
