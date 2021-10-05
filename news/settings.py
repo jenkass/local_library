ca8c9cd6a561b774f457f49166b8a0b95ea9f7fe
Bond <bondarenko.jenek@mail.ru>
2021-10-06 01:33:16 +0300

    edited the configuration of project

--- news/settings.py
+++ news/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-og=+*8_q#(+!!0(mxcjfdb&0xcd!^e90=zwk^-^+c&r9_g@%-i'
+#SECRET_KEY = 'django-insecure-og=+*8_q#(+!!0(mxcjfdb&0xcd!^e90=zwk^-^+c&r9_g@%-i'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
